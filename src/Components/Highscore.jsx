671c7b73ca54792c169562b9b5a53a5ef5d0c3e3
preetpalsinghkanda <preetpalsinghkanda@gmail.com>
2026-03-23 18:57:12 +0530

    added HighScore-Component

--- src/Components/Highscore.jsx
+++ src/Components/Highscore.jsx
@@ -2,30 +2,35 @@ import React from "react";
 import "./Highscore.css";
 import smashedIcon from '../assets/icon-new-pb.svg';
 import restartIcon from '../assets/icon-restart.svg';
+import patternConfettiIcon from '../assets/pattern-confetti.svg';
 
 
 
 export default function Highscore(){
     return(
+        <>
         <div className="highscore-container">
-            <div><img src={smashedIcon} alt="smashedIcon" /></div>
-            <div><h2>High Score Smashed!</h2><p>You're getting faster. That was incredible typing.</p></div>
+            
+            <img src={smashedIcon} alt="smashedIcon" className="smashedIcon"/>
+            <div className="highscore-heading"><h2>High Score Smashed!</h2><p>You're getting faster. That was incredible typing.</p></div>
             <div className="highscore-result">
-                <div>
+                <div className="wpm">
                     <p>WPM:</p>
-                    <span></span>
+                    <span>95</span>
                 </div>
-                <div>
+                <div className="accuracy">
                     <p>Accuracy:</p>
-                    <span></span>
+                    <span><span>100</span>%</span>
                 </div>
-                <div>
-                    <p>Characters</p>
-                    <span></span>
+                <div className="characters">
+                    <p>Characters:</p>
+                    <span><span className="correct-char">120</span>/<span className="count">5</span></span>
                 </div>
             </div>
-            <button>Beat This Score <img src={restartIcon} alt="restartIcon" className="restartIcon"/></button>
+            <button className="BeatThisScore-btn">Beat This Score <img src={restartIcon} alt="restartIcon" className="restartIcon"/></button>
+            
         </div>
-
+        <img src={patternConfettiIcon} alt="patternConfettiIcon" className="patternConfettiIcon" />
+</>
     )
 }
